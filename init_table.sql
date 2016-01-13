e14728bb1db0ba6ff4be118ea992c9e057f8a4aa
Vadim Voznyuk <vadya90@gmail.com>
2016-01-13 14:11:57 +0300

    Delete init_table.sql

--- init_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE BRANCH_REF
-(  "ID" NUMBER(22,0) NOT NULL ,		--	Идентификатор записи в таблице
-	"PARENT_ID" NUMBER(22,0),	-- Идентификатор родительского подразделения
-	"NAME" VARCHAR2(240) NOT NULL,	--	Название подразделения
-	"CODE" VARCHAR2(40) NOT NULL,	--	Код подразделения
-	"CHANGE_TIME" DATE,	-- Дата изменения записи в таблице
-	CONSTRAINT BRANCH_PK PRIMARY KEY (ID)
-)
